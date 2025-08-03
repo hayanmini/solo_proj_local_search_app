fbb9db8984edb289b001f3de5c4da84c6d1ebf5b
hayanmini <dudals9696@gmail.com>
2025-08-03 15:26:56 +0900

    리뷰 모델 생성 완료
    - fromJson, toJson 구현 완료

---
+++ lib/data/model/review.dart
@@ -0,0 +1,32 @@
+class Review {
+  String id;
+  String content;
+  double mapx;
+  double mapy;
+  DateTime createdAt;
+
+  Review({
+    required this.id,
+    required this.content,
+    required this.mapx,
+    required this.mapy,
+    required this.createdAt,
+  });
+
+  Review.fromJson(Map<String, dynamic> json)
+    : this(
+        id: json["id"],
+        content: json["content"],
+        mapx: json["mapx"],
+        mapy: json["mapy"],
+        createdAt: DateTime.parse(json["createdAt"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "content": content,
+    "mapx": mapx,
+    "mapy": mapy,
+    "createdAt": createdAt,
+  };
+}
